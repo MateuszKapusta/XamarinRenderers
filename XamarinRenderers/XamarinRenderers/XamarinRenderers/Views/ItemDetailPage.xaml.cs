9cb2ec2ae0d3aa3429cd995526b25b4c1a8c4280
Mateusz Kapusta <mateusz6527@gmail.com>
2023-10-21 20:35:46 +0200

    App init

---
+++ XamarinRenderers/XamarinRenderers/XamarinRenderers/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinRenderers.ViewModels;
+
+namespace XamarinRenderers.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
